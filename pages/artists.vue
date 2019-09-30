0ac103aec1e9519922921525af43e5741909aa03
Carles <carles2612@gmail.com>
2019-09-30 19:05:36 +0200

    Add artists page

---
+++ pages/artists.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="artists">
+    <div class="artists-picture-container">
+      <div class="artists-picture"></div>
+    </div>
+    <div class="artists-list">
+      <h2>Artistes</h2>
+      <ul>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+        <li><nuxt-link to='/'>Artistsaasd</nuxt-link></li>
+      </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '../sass/variables';
+
+  .artists{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas: "picture list";
+    grid-gap: 3rem;
+    margin-top: 2rem;
+
+    &-picture {
+      grid-area: picture;
+      margin-top: 10vh;
+      background: red;
+      height: 60vh;
+      position: sticky;
+      top: 2rem;
+    }
+
+    &-list {
+      grid-area: list;
+
+      ul {
+        padding: 0;
+      }
+      li {
+        font-size: 3rem;
+        list-style: none;
+        padding: 0;
+      }
+      a {
+        &::after {
+          display: none;
+        }
+        &:hover {
+          color: $text;
+        }
+      }
+    }
+  }
+
+  @include media-breakpoint-down(md) {
+    .artists {
+      grid-template-areas: "list";
+      grid-template-columns: 1fr;
+
+      &-picture-container {
+        display: none;
+      }
+    }
+
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <logo />
+
       <h1 class="title">
         Sonora
       </h1>
