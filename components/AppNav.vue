36b40dee62325ad47ea8a89aa6d2f370c6b8e879
Carles <carles2612@gmail.com>
2019-10-02 17:22:09 +0200

    Add contact button to nav

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -17,6 +17,9 @@
         <b-nav-item to='/about'>
           About
         </b-nav-item>
+        <b-nav-item to='/contact'>
+          Contacte
+        </b-nav-item>
       </b-navbar-nav>
 
       <b-navbar-nav class="ml-auto">
