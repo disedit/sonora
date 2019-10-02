19b0c29c740278f4d2a7218181b5e17c1f8c887b
Carles <carles2612@gmail.com>
2019-10-02 18:10:42 +0200

    Add artist pictures and update artist list

--- pages/artists/artists.js
+++ pages/artists/artists.js
@@ -1,5 +1,23 @@
 import BorrianaBigBandImg from '@/assets/images/artists/borriana-big-band.jpg'
 import CantaCanallaImg from '@/assets/images/artists/canta-canalla.jpg'
+import CarlesPastorImg from '@/assets/images/artists/carles-pastor.jpg'
+import DaniMiquelImg from '@/assets/images/artists/dani-miquel.jpg'
+import ElsJovensImg from '@/assets/images/artists/els-jovens.jpg'
+import FridaImg from '@/assets/images/artists/frida.jpg'
+import FruKatinkaImg from '@/assets/images/artists/fru-katinka.jpg'
+import JohnnyBZeroImg from '@/assets/images/artists/johnny-b-zero.jpg'
+import JonatanPenalbaImg from '@/assets/images/artists/jonatan-penalba.jpg'
+import JrMackenzieImg from '@/assets/images/artists/jr-mackenzie.jpg'
+import JuliaClaraAndresImg from '@/assets/images/artists/julia-clara-andres.jpg'
+import JunesKaleidoscopeImg from '@/assets/images/artists/junes-kaleidoscope.jpg'
+import NovembreElectricImg from '@/assets/images/artists/novembre-electric.jpg'
+import PauAlabajosImg from '@/assets/images/artists/pau-alabajos.jpg'
+import PavvlaImg from '@/assets/images/artists/pavvla.jpg'
+import ProzakSoupImg from '@/assets/images/artists/prozak-soup.jpg'
+import RascanyaImg from '@/assets/images/artists/rascanya.jpg'
+import SanteroImg from '@/assets/images/artists/santero-y-los-muchachos.jpg'
+import TrobadoretsImg from '@/assets/images/artists/trobadorets.jpg'
+import VeraGreenImg from '@/assets/images/artists/vera-green.jpg'
 
 const artists = [
   {
@@ -11,7 +29,100 @@ const artists = [
     id: 'canta-canalla',
     name: 'Canta Canalla',
     image: CantaCanallaImg
+  },
+  {
+    id: 'carles-pastor',
+    name: 'Canta Canalla',
+    image: CarlesPastorImg
+  },
+  {
+    id: 'dani-miquel',
+    name: 'Dani Miquel',
+    image: DaniMiquelImg
+  },
+  {
+    id: 'els-jovens',
+    name: 'Els Jóvens',
+    image: ElsJovensImg
+  },
+  {
+    id: 'frida',
+    name: 'Frida',
+    image: FridaImg
+  },
+  {
+    id: 'fru-katinka',
+    name: 'Fru Katinka',
+    image: FruKatinkaImg
+  },
+  {
+    id: 'johnny-b-zero',
+    name: 'Johnny B. Zero',
+    image: JohnnyBZeroImg
+  },
+  {
+    id: 'jonatan-penalba',
+    name: 'Jonatan Penalba',
+    image: JonatanPenalbaImg
+  },
+  {
+    id: 'jr-mackenzie',
+    name: 'Junior Mackenzie',
+    image: JrMackenzieImg
+  },
+  {
+    id: 'julia-clara-andrés',
+    name: 'Júlia i Clara Andrés',
+    image: JuliaClaraAndresImg
+  },
+  {
+    id: 'junes-kaleidoscope',
+    name: "June's Kaleidoscope",
+    image: JunesKaleidoscopeImg
+  },
+  {
+    id: 'novembre-electric',
+    name: 'Novembre Elèctric',
+    image: NovembreElectricImg
+  },
+  {
+    id: 'pau-alabajos',
+    name: 'Pau Alabajos',
+    image: PauAlabajosImg
+  },
+  {
+    id: 'pavvla',
+    name: 'Pavvla',
+    image: PavvlaImg
+  },
+  {
+    id: 'prozak-soup',
+    name: 'Prozak Soup',
+    image: ProzakSoupImg
+  },
+  {
+    id: 'rascanya',
+    name: 'Rascanya',
+    image: RascanyaImg
+  },
+  {
+    id: 'santero-y-los-muchachos',
+    name: 'Santero y los Muchachos',
+    image: SanteroImg
+  },
+  {
+    id: 'trobadorets',
+    name: 'Trobadorets',
+    image: TrobadoretsImg
+  },
+  {
+    id: 'vera-green',
+    name: 'VeraGreen',
+    image: VeraGreenImg
   }
 ]
+
+
+
 
 export default artists
