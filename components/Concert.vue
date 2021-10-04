c6d8ec643e9c7ec8605af8f36a06928256bddfa8
guillem <guillem@disedit.com>
2021-10-04 17:17:59 +0200

    Update concert tickets btn

--- components/Concert.vue
+++ components/Concert.vue
@@ -12,13 +12,19 @@
       {{ concert.date | niceDate }}<br>
       {{ concert.venue }}
     </div>
-    <div v-if="concert.tickets_url" class="concert-book">
+    <div class="concert-book">
       <span v-if="dimmed">
         CONCERT REALITZAT
       </span>
-      <a v-else :href="concert.tickets_url" class="btn" target="_blank" rel="noopener noreferer">
+      <a v-else-if="concert.tickets_url" :href="concert.tickets_url" class="btn" target="_blank" rel="noopener noreferer">
         {{ concert.button }}
       </a>
+      <span v-else-if="concert.municipality === 'bocairent'" class="contact-text">
+        Per reservar les vostres localitats, envieu un email a <a href="mailto:info@bocairent.es">info@bocairent.es</a> o truqueu al telèfon <a href="tel:962350014">962 35 00 14</a>
+      </span>
+      <span v-else class="faux-btn">
+        Entrades a la venda properament
+      </span>
     </div>
   </div>
 </template>
@@ -84,7 +90,8 @@ export default {
     display: flex;
     align-items: center;
 
-    a {
+    .btn,
+    .faux-btn {
       display: block;
       border: 1px $black solid;
       text-transform: uppercase;
@@ -93,11 +100,27 @@ export default {
       text-align: center;
       transition: .25s ease;
       flex-grow: 1;
-
-      &:hover {
-        background: $white;
-      }
+      line-height: 1;
+      height: 3.375rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 1rem;
     }
+
+    .btn:hover {
+      background: $white;
+    }
+  }
+}
+
+.contact-text {
+  line-height: 1.1;
+  font-size: 0.9rem;
+
+  a {
+    color: $black;
+    text-decoration: underline;
   }
 }
 
