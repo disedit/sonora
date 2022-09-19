15f6f9bcadac79a7fd08bf28e343b4be9aed00b5
guillem <guillem@disedit.com>
2022-09-19 16:27:13 +0200

    Update contentful envs

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,6 +1,6 @@
 const contentful = require('contentful')
 
 module.exports = contentful.createClient({
-  space: process.env.NUXT_ENV_CTF_SPACE_ID,
-  accessToken: process.env.NUXT_ENV_CTF_ACCESS_TOKEN
+  space: process.env.NUXT_ENV_CTF_SPACE_ID_2022,
+  accessToken: process.env.NUXT_ENV_CTF_ACCESS_TOKEN_2022
 })
